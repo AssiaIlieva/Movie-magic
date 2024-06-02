05d42280ac7130de5b6e407a113891e1888ea477
AssiaIlieva <ac17@abv.bg>
2024-06-02 18:38:40 +0300

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,14 @@
-const movies = [];
+const movies = [{
+    name: 'Jungle Cuise',
+    genre: 'Adventrue',
+    dirctor: 'Lukas',
+    year: '2019',
+    imageURL: '/img/jungle-cruise.jpeg',        
+    rating: '5',
+    description: "The youngest of King Triton's"
+  }];
+  
 exports.create = (movieData) => {
+    console.log(movieData);
     movies.push(movieData);
 }
