9ae8b154c3c7832e72cca897022bd7ef84089b3c
AssiaIlieva <ac17@abv.bg>
2024-06-03 16:21:39 +0300

    Add rating to the details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -14,7 +14,8 @@ router.post('/create', (req, res) => {
 router.get('/movies/:movieId', (req, res) => {
     const movieId = req.params.movieId;
     const movie = movieService.getOne(movieId);
-    console.log(movie);
+    // TODO: to be changed with handlebars helpers
+    movie.ratingStars = '&#x2605'.repeat(movie.rating);
     res.render('details', {movie});
 });
 
