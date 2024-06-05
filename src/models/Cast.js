d144852c396bcdf66a092612c5d54dac670859b6
AssiaIlieva <ac17@abv.bg>
2024-06-05 11:29:42 +0300

    Create Cast model

---
+++ src/models/Cast.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const { validate } = require('./Movie');
+
+const castSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true,
+        max: 120,
+        min: 5
+    },
+    born: {
+        type: String,
+        required: true
+    },
+    nameInMovie: {
+        type: String,
+        required: true
+    },
+    castImage: {
+        type: String,
+        required: true,
+        validate: {
+            validator(value){
+                return /^https?:\/\/.+/.test(value);
+            },
+            message: (props) => `${props.value} is invlid url for the castImage!`
+        }
+    }
+})
+
+const Cast = mongoose.model('Cast', castSchema);
+
+module.exports = Cast
