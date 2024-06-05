96b40f15e8a4fa0335db81174d62e1decec5d863
AssiaIlieva <ac17@abv.bg>
2024-06-05 09:27:04 +0300

    Refactor movieService to use Movie model

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -33,7 +33,7 @@ const movieSchema = new mongoose.Schema({
     imageURL: {
         type: String,
         required: true,
-        match: /^https?/
+        match: /^https?:\/\/.+/
     },
 });
 
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,27 +1,12 @@
 const Movie = require('../models/Movie')
 
-const movies = [{
-    _id: 1,
-    title: 'Jungle Cuise',
-    genre: 'Adventure',
-    dirctor: 'Lukas',
-    year: '2019',
-    imageURL: '/img/jungle-cruise.jpeg',        
-    rating: '5',
-    description: "The youngest of King Triton's"
-  }];
+exports.getAll = () => Movie.find();
 
-exports.getAll = () => {
-    return movies.slice();
-};
+exports.getOne = (movieId) => Movie.findById(movieId);
 
-exports.getOne = (movieId) => {
-    const movie = movies.find(m => m._id == movieId);
-    return movie;
-}
-
-exports.search = (title, genre, year) => {
-result = movies.slice();
+// TODO:Filter result in mongoDB
+exports.search = async (title, genre, year) => {
+result = await Movie.find().lean()
 
     if(title){
         result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
@@ -38,7 +23,4 @@ result = movies.slice();
     return result
 }
 
-exports.create = async (movieData) => {
-    const result = await Movie.create(movieData);
-    return result;
-};
+exports.create = (movieData) => Movie.create(movieData);
