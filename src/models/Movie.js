2badd1c46166652569a66ca7dad8bdbb9a956de2
AssiaIlieva <ac17@abv.bg>
2024-06-12 18:11:36 +0300

    Add validations in Movie model

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -4,38 +4,38 @@ const movieSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, 'All fields are required'],
-        match: [/^[A-Za-z0-9\s]+$/, 'Incorrect characters in the field'],
-        minLength: [5, 'Should be at least 5 characters']
+        match: [/^[A-Za-z0-9\s]+$/, 'Incorrect characters in the field title'],
+        minLength: [5, 'Title should be at least 5 characters']
     },
     genre: {
         type: String,
         required: [true, 'All fields are required'],
-        minLength: [5, 'Should be at least 5 characters'],
-        match: [/^[A-Za-z0-9\s]$/, 'Incorrect characters in the field'],
+        minLength: [5, 'Genre should be at least 5 characters'],
+        match: [/^[A-Za-z0-9\s]+$/, 'Incorrect characters in the field genre'],
     },
     director: {
         type: String,
         required: [true, 'All fields are required'],
-        minLength: [5, 'Should be at least 5 characters'],
-        match: [/^[A-Za-z0-9\s]$/, 'Incorrect characters in the field'],
+        minLength: [5, 'Director should be at least 5 characters'],
+        match: [/^[A-Za-z0-9\s]+$/, 'Incorrect characters in the field director'],
     },
     year: {
         type: Number,
         required: [true, 'All fields are required'],
-        min: [1900, 'Must be between 1900 and 2024'],
-        max: [2024, 'Must be between 1900 and 2024']
+        min: [1900, 'Year must be between 1900 and 2024'],
+        max: [2024, 'Year must be between 1900 and 2024']
     },
     rating: {
         type: Number,
         required: [true, 'All fields are required'],
-        min: [1, 'Must be between 1 and 5'],
-        max: [5, 'Must be between 1 and 5']
+        min: [1, 'Rating must be between 1 and 5'],
+        max: [5, 'Rating must be between 1 and 5']
     },
     description: {
         type: String,
         required: [true, 'All fields are required'],
-        minLength: [20, 'Should be at least 20 characters'],
-        match: [/^[A-Za-z0-9\s]$/, 'Incorrect characters in the field'],
+        minLength: [20, 'Description should be at least 20 characters'],
+        match: [/^[A-Za-z0-9\s]+$/, 'Incorrect characters in the field description'],
     },
     imageURL: {
         type: String,
