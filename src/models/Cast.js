c54a2191d77e22467cbf63be2ab1729663884fea
AssiaIlieva <ac17@abv.bg>
2024-06-12 18:19:50 +0300

    Update the validation in Cast model

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -4,26 +4,26 @@ const castSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'All fields are required'],
-        minLength: [5, 'Should be at least 5 characters'],
-        match: [/^[A-Za-z0-9\s]$/, 'Incorrect characters in the field'],
+        minLength: [5, 'Name should be at least 5 characters'],
+        match: [/^[A-Za-z0-9\s]+$/, 'Incorrect characters in the Name field'],
     },
     age: {
         type: Number,
         required: [true, 'All fields are required'],
-        max: [120, 'Must be between 1 and 120'],
-        min: [1, 'Must be between 1 and 120']
+        max: [120, 'Age must be between 1 and 120'],
+        min: [1, 'Age must be between 1 and 120']
     },
     born: {
         type: String,
         required: [true, 'All fields are required'],
         minLength: [10, 'Should be at least 10 characters'],
-        match: [/^[A-Za-z0-9\s]$/, 'Incorrect characters in the field'],
+        match: [/^[A-Za-z0-9\s]+$/, 'Incorrect characters in the Born field'],
     },
     nameInMovie: {
         type: String,
         required: [true, 'All fields are required'],
-        minLength: [5, 'Should be at least 5 characters'],
-        match: [/^[A-Za-z0-9\s]$/, 'Incorrect characters in the field'],
+        minLength: [5, 'Name in Movie should be at least 5 characters'],
+        match: [/^[A-Za-z0-9\s]+$/, 'Incorrect characters in the Name in Movie field'],
     },
     castImage: {
         type: String,
